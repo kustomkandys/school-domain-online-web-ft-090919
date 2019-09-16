2e6bbc417624b6fc343778ef43fa46123150725f
Keri Deemer <kerid18@att.net>
2019-09-16 15:11:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,10 @@ class School
   end 
   
   def add_student(name, grade)
-    @roster["grade"] = []
-    roster["grade"] << name
+    if @roster["grade"] = []
+      @roster["grade"] << :name
+    else 
+      @roster["grade"] = [:name]
   end
   
   def grade=(grade)
