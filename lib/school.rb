a9c8f01853989e515d4005b305d8fb9b579d50be
Keri Deemer <kerid18@att.net>
2019-09-16 15:47:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,5 @@
 class School
-  attr_accessor :roster
+  attr_accessor :roster, :student_name
   attr_reader :name 
   
   def initialize(name)
@@ -8,7 +8,7 @@ class School
   end 
   
   def add_student(student_name, grade)
-    @roster[grade] =[] ||= @roster[grade] = [student_name]
+    @roster[grade] ||= @roster[grade] = [student_name]
   end
   
   def grade(grade)
